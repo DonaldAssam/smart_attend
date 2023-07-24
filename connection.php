3c0981943b80cadfae17fe716df1ec51eef52f84
DonaldAssam <donaldassam@gmail.com>
2023-07-24 12:08:53 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -5,7 +5,7 @@
 
 
 try {
-    $conn = new PDO('mysql:host = localhost; dbname=biometric', $username, $password);
+    $conn = new PDO("mysql:host = $servername; dbname=biometric", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //echo "Connected successfully"; 
